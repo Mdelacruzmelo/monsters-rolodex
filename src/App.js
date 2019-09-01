03de5b725208cdb103d8068c17d156882ba2c57f
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2019-09-01 21:19:01 +0200

    fetch promises

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,21 +9,16 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asc3'
-        },
-      ]
+      monsters: []
     };
+
+  }
+
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json()
+        .then(users => this.setState({ monsters: users })));
 
   }
 
