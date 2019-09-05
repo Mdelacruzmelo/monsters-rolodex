3a27a755257dfb8633e36725f44bded078062e88
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2019-09-05 21:34:56 +0200

    Updates

---
+++ precache-manifest.b44a4cf2b1b23edb36023aeaa2734761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f82b526cf837e2611c0be9ca84a5a2e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "168c906ae9d6e7f1b363",
+    "url": "/monsters-rolodex/static/css/main.6d7635a9.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monsters-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "168c906ae9d6e7f1b363",
+    "url": "/monsters-rolodex/static/js/main.6b4c6375.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
